7be4f4d1fdc7fc3aacc3729d92ae5ed7cb1bbf4f
Sentiax <devtools877@gmail.com>
2023-11-07 22:46:06 +0300

    Update twiz.js to `1.0.1 Beta`

--- twiz.js
+++ twiz.js
@@ -1,14 +1,11 @@
 window.onload = function() {
     document.body.innerHTML += `
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
     <style> 
-    @font-face {
-        font-family: 'Satoshi-Font';
-        src: url(https://cdn.jsdelivr.net/gh/Twiz-lol/Twast@main/SatoshiBlack.ttf);
-    }
     .notification {
         font-family: 'Satoshi-Font';
     }
@@ -28,14 +25,9 @@ window.onload = function() {
     }
 
     .notification_err {
-        position: fixed;
-        top: 2%;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 50%;
-        max-width: fit-content;
-        border: 2px solid rgb(255 0 0 / 44%);
-        background-color: rgb(255 0 0 / 11%);
+        position: relative;
+        padding: 2%;
+        max-width: fit-content; 
         color: white;
         border-radius: 10px;
         padding: 10px;
@@ -43,10 +35,13 @@ window.onload = function() {
         align-items: center;
         justify-content: center;
         font-family: Arial, sans-serif;
-        backdrop-filter: blur(5px); /* Add a bit of blur to the background */
+        backdrop-filter: blur(5px);
+        margin-bottom: 10px; 
+        border: 2px solid rgb(255 0 0 / 44%);
+        background-color: rgb(255 0 0 / 11%);
     }
 
-    .icon-err {
+    .icon-error {
         font-size: 20px;
         margin-right: 10px;
         color: #904545;
@@ -54,11 +49,8 @@ window.onload = function() {
         padding: 6px;
     }
     .notification_warn {
-        position: fixed;
-        top: 2%;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 40%;
+        position: relative;
+        padding: 2%;
         max-width: fit-content;
         border: 2px solid rgb(251 255 0 / 44%);
         background-color: rgb(254 255 0 / 19%);
@@ -70,14 +62,12 @@ window.onload = function() {
         justify-content: center;
         font-family: Arial, sans-serif;
         backdrop-filter: blur(5px);
+        margin-bottom: 10px; 
     }
 
-    .notification {
-        position: fixed;
-        top: 2%;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 50%;
+    .notification-success {
+        position: relative;
+        padding: 2%;
         max-width: fit-content;
         border: 2px solid rgb(0 255 0 / 33%);
         background-color: rgb(19 255 29 / 14%);
@@ -88,10 +78,11 @@ window.onload = function() {
         align-items: center;
         justify-content: center;
         font-family: Arial, sans-serif;
-        backdrop-filter: blur(5px); /* Add a bit of blur to the background */
+        backdrop-filter: blur(5px);
+        margin-bottom: 10px; 
     }
-
-    .icon {
+    
+    .icon-success {
         font-size: 20px;
         margin-right: 10px;
         color: #529152;
@@ -111,82 +102,119 @@ window.onload = function() {
     .notification:hover .close-button {
         display: block;
     }
+    /* on top center */
+    .notification-stack {
+        position: fixed;
+        top: 20px;
+        left: 50%;
+            transform: translateX(-50%);
+            display: flex;
+            flex-direction: column-reverse;
+    align-items: center;
+        gap: 10px !Important;
+        z-index: 9999 !Important;
+        min-width: 200px;
+    max-width: 300px;
+    }
+    /* on bottom center */
+    .notification-stack1 {
+        position: fixed;
+        bottom: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+        display: flex !important;
+        flex-direction: column-reverse;
+    align-items: center;
+        gap: 10px !important;
+        z-index: 9999 !important;
+        width: fit-content;
+        min-width: 200px; 
+    max-width: 300px;
+    }
+    /* on bottom right */
+    .notification-stack2 {
+        position: fixed;
+    bottom: 20px;
+    right: 20px;
+    display: flex !important;
+    flex-direction: column;
+            align-items: flex-end;
+    gap: 10px !important;
+    z-index: 9999 !important;
+    width: fit-content;
+    min-width: 200px; 
+    max-width: 300px;
+    }
 
     .message {
-        font-size: 15px; /* Adjust the font size of the text to make it smaller */
+        font-size: 15px; 
         font-family: 'Satoshi-Font';
-        color: #ffffffb5;
         font-size: smaller;
     }</style>`;
 
 }
-function fire(error_sucess_warn, message) {
-    const notifications = document.querySelectorAll('.notification');
-    const notifications1 = document.querySelectorAll('.notification_err');
-    const notifications2 = document.querySelectorAll('.notification_warn');
-    notifications.forEach(notification => {
-    notification.remove();
-    });
-    notifications1.forEach(notification => {
-        notification.remove();
-        });
-    notifications2.forEach(notification => {
-            notification.remove();
-            });
-    if (error_sucess_warn == "error") {
-        document.body.innerHTML += `<div class="notification_err animate__animated animate__slideInDown animate__faster"  id="notification">
+function fire(error_sucess_warn, message, time, icon, position) {
+    const notificationContainer = document.querySelector('.notification-stack');
+    const notificationContainer1 = document.querySelector('.notification-stack1');https://hub.tabnine.com/v9/support
+    const notificationContainer2 = document.querySelector('.notification-stack2');
+    if (!notificationContainer || !notificationContainer1 || !notificationContainer2) {
+        console.log(position)
+        if (position) {
+        if (position == "bottom-center") {
+        document.body.innerHTML += `<div class="notification-stack1"></div>`;
+    } else if (position == "bottom-right") {
+        document.body.innerHTML += `<div class="notification-stack2"></div>`;
+    } else {
+        document.body.innerHTML += `<div class="notification-stack"></div>`;
+    }
+} else {
+    document.body.innerHTML += `<div class="notification-stack"></div>`;
+}
+    }
+
+    const notificationTypeClass = {
+        error: 'notification_err',
+        success: 'notification-success',
+        warn: 'notification_warn',
+    }[error_sucess_warn];
+
+    const notificationElement = document.createElement('div');
+    notificationElement.classList.add(notificationTypeClass, 'animate__animated', 'animate__slideInDown', 'animate__faster');
+
+    const iconElement = document.createElement('div');
+    iconElement.classList.add(`icon-${error_sucess_warn}`);
+    if (!icon) {
+    iconElement.innerHTML = '<i class="fas fa-check-circle"></i>';
+    } else iconElement.innerHTML = `${icon}`;
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.innerHTML = message;
+
+    notificationElement.appendChild(iconElement);
+    notificationElement.appendChild(messageElement);
+    if (position) {
+    if (position == "bottom-center") {
+    document.querySelector('.notification-stack1').appendChild(notificationElement);
+} else if (position == "bottom-right") {
+    document.querySelector('.notification-stack2').appendChild(notificationElement);
+} else {
+    document.querySelector('.notification-stack').appendChild(notificationElement);
+}
+    } else {
+    document.querySelector('.notification-stack').appendChild(notificationElement);
         
-        <div class="icon-err">
-            <i class="fas fa-check-circle"></i>
-        </div>
-        <div class="message">
-            ${message}
-        </div>
-    </div>`
     }
-    if (error_sucess_warn == "success") {
-        document.body.innerHTML += `<div class="notification animate__animated animate__slideInDown animate__faster" id="notification">
-
-        <div class="icon">
-            <i class="fas fa-check-circle"></i>
-        </div>
-        <div class="message">
-            ${message}
-        </div>
-    </div>`
+    if (time) {
+        setTimeout(() => {
+            closeNotification(notificationElement);
+        }, time); 
+    }  else {
+        setTimeout(() => {
+            closeNotification(notificationElement);
+        }, 6000); 
     }
-    if (error_sucess_warn == "warn") {
-        document.body.innerHTML += `<div class="notification_warn animate__animated animate__slideInDown animate__faster" id="notification">
-        <!-- <span class="close-button" onclick="closeNotification()"> 
-            <i class="fas fa-times"></i>
-        </span>-->
-        <div class="icon-warn">
-            <i class="fas fa-check-circle"></i>
-        </div>
-        <div class="message">
-            ${message}
-        </div>
-    </div>`
-    }
-setTimeout(closeNotification, 6000); 
 }
 
-function closeNotification() {
-    const notifications = document.querySelectorAll('.notification');
-    const notifications1 = document.querySelectorAll('.notification_err');
-    const notifications2 = document.querySelectorAll('.notification_warn');
-    notifications.forEach(notification => {
-    notification.remove();
-    });
-    notifications1.forEach(notification => {
-        notification.remove();
-        });
-    notifications2.forEach(notification => {
-            notification.remove();
-            });
+function closeNotification(notificationElement) {
+    notificationElement.remove();
 }
-
-/*  CopyRight®️ 
-Developer twiz.lol/@2 
-FrontEnd Developer twiz.lol/@sember
-*/
